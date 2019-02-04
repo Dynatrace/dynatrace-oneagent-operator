0a4b5440d9f67f11b502df913d6a7a2cb0944a1b
Bernhard Aichinger <bernhard.aichinger@dynatrace.com>
2019-02-04 10:48:51 +0100

    Rerun code generation for defaulters

--- pkg/apis/dynatrace/v1alpha1/doc.go
+++ pkg/apis/dynatrace/v1alpha1/doc.go
@@ -1,5 +1,5 @@
 // Package v1alpha1 contains API Schema definitions for the dynatrace v1alpha1 API group
 // +k8s:deepcopy-gen=package,register
-// +k8s:defaulter-gen=OneAgentSpec
+// +k8s:defaulter-gen=TypeMeta
 // +groupName=dynatrace.com
 package v1alpha1
