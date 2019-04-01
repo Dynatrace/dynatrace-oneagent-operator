8450e45df5c766072049ff7ee80185d967222867
Namrata <chdhry.namrata@gmail.com>
2019-04-01 13:27:38 +0200

    change test name + remove specifying deamon spec

--- pkg/controller/oneagent/oneagent_controller_test.go
+++ pkg/controller/oneagent/oneagent_controller_test.go
@@ -6,40 +6,23 @@ import (
 
 	dynatracev1alpha1 "github.com/Dynatrace/dynatrace-oneagent-operator/pkg/apis/dynatrace/v1alpha1"
 	appsv1 "k8s.io/api/apps/v1"
-	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/kubernetes/scheme"
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 )
 
-func TestOneAgentController(t *testing.T) {
-
+func TestReconcileOneAgent_ReconcileOnEmptyEnvironment(t *testing.T) {
 	var (
 		name      = "dynatrace-oneagent"
 		namespace = "dynatrace"
 	)
 
 	oa := newOneAgentSpec()
+	oa.ApiUrl = "https://ENVIRONMENTID.live.dynatrace.com/api"
+	oa.Tokens = "token_test"
 	dynatracev1alpha1.SetDefaults_OneAgentSpec(oa)
-	if oa.ApiUrl == "" {
-		oa.ApiUrl = "https://ENVIRONMENTID.live.dynatrace.com/api"
-	}
-	oa.Tokens = "token123"
-	ds := newDaemonSetSpec()
-	ds.Template.Spec.Containers = []corev1.Container{{
-		Image:     "docker.io/dynatrace/oneagent",
-		Args:      []string{"INFRO_ONLY=1"},
-		Resources: newResourceRequirements(),
-		Env:       newEnvVar(),
-	}}
-	ds.Template.Spec.Tolerations = []corev1.Toleration{}
-	ds.Template.Spec.NodeSelector = map[string]string{"k": "v"}
-	ds.Template.Spec.PriorityClassName = "class"
-
-	copyDaemonSetSpecToOneAgentSpec(ds, oa)
 
 	instance := &dynatracev1alpha1.OneAgent{
 		ObjectMeta: metav1.ObjectMeta{
@@ -48,14 +31,11 @@ func TestOneAgentController(t *testing.T) {
 		},
 		Spec: *oa,
 	}
-	objs := []runtime.Object{
-		instance,
-	}
 
 	scheme := scheme.Scheme
 	scheme.AddKnownTypes(dynatracev1alpha1.SchemeGroupVersion, instance)
 
-	client := fake.NewFakeClient(objs...)
+	client := fake.NewFakeClient(instance)
 	// reconcile oneagent
 	reconcileOA := &ReconcileOneAgent{client: client, scheme: scheme}
 
@@ -71,7 +51,7 @@ func TestOneAgentController(t *testing.T) {
 		t.Fatalf("error reconciling : %v", err)
 	}
 
-	// Check if deamonset has been created and has the correct size.
+	// Check if deamonset has been created and has correct namespace and name.
 	dsActual := &appsv1.DaemonSet{}
 	err = client.Get(context.TODO(), types.NamespacedName{Name: name, Namespace: namespace}, dsActual)
 	if err != nil {
