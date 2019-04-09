1e04480cf6366c1dc83e19d2df220ecc42e3e799
Namrata <chdhry.namrata@gmail.com>
2019-04-09 12:42:57 +0200

    add failure case for istio helper test

--- pkg/controller/istio/helper_test.go
+++ pkg/controller/istio/helper_test.go
@@ -11,29 +11,19 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-func initMockServer(t *testing.T) *httptest.Server {
+func initMockServer(t *testing.T, list *metav1.APIGroupList) *httptest.Server {
 
 	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-		var list interface{}
+		var resources interface{}
 		switch req.URL.Path {
 		case "/apis":
-			list = &metav1.APIGroupList{
-				Groups: []metav1.APIGroup{
-					{
-						Name: "networking.istio.io",
-						Versions: []metav1.GroupVersionForDiscovery{
-							{GroupVersion: "v1alpha3", Version: "v1alpha3"},
-							{GroupVersion: "v1alpha3", Version: "v1alpha3"},
-						},
-					},
-				},
-			}
+			resources = list
 		default:
-			t.Logf("unexpected request: %s", req.URL.Path)
+			// t.Logf("unexpected request: %s", req.URL.Path)
 			w.WriteHeader(http.StatusNotFound)
 			return
 		}
-		output, err := json.Marshal(list)
+		output, err := json.Marshal(resources)
 		if err != nil {
 			t.Errorf("unexpected encoding error: %v", err)
 			return
@@ -48,13 +38,50 @@ func initMockServer(t *testing.T) *httptest.Server {
 
 func TestIstioEnabled(t *testing.T) {
 
-	server := initMockServer(t)
-	defer server.Close()
+	{
+		// resource is enabled
+		list := &metav1.APIGroupList{
+			Groups: []metav1.APIGroup{
+				{
+					Name: "networking.istio.io",
+					Versions: []metav1.GroupVersionForDiscovery{
+						{GroupVersion: "v1alpha3", Version: "v1alpha3"},
+						{GroupVersion: "v1alpha3", Version: "v1alpha3"},
+					},
+				},
+			},
+		}
+		server := initMockServer(t, list)
+		defer server.Close()
+		cfg := &restclient.Config{Host: server.URL}
 
-	cfg := &restclient.Config{Host: server.URL}
-	r, e := CheckIstioEnabled(cfg)
-	if r != true {
-		t.Fail()
-		t.Error(e)
+		r, e := CheckIstioEnabled(cfg)
+		if r != true {
+			t.Fail()
+			t.Error(e)
+		}
+	}
+	{
+		// resource is not enabled
+		list := &metav1.APIGroupList{
+			Groups: []metav1.APIGroup{
+				{
+					Name: "not.istio.group",
+					Versions: []metav1.GroupVersionForDiscovery{
+						{GroupVersion: "v1alpha3", Version: "v1alpha3"},
+						{GroupVersion: "v1alpha3", Version: "v1alpha3"},
+					},
+				},
+			},
+		}
+		server := initMockServer(t, list)
+		defer server.Close()
+		cfg := &restclient.Config{Host: server.URL}
+
+		r, e := CheckIstioEnabled(cfg)
+		if r != false {
+			t.Fail()
+			t.Errorf("expected false, got true, %v", e)
+		}
 	}
 }
