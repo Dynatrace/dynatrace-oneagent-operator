9524014fe34c1dd58f8ff77b13250e397ad83d4b
Namrata <chdhry.namrata@gmail.com>
2019-04-09 12:24:04 +0200

    add unit test for istio helper

---
+++ pkg/controller/istio/helper_test.go
@@ -0,0 +1,61 @@
+package istio
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/Dynatrace/dynatrace-oneagent-operator/pkg/controller/istio"
+	restclient "k8s.io/client-go/rest"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func initMockServer(t *testing.T) *httptest.Server {
+
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		var list interface{}
+		switch req.URL.Path {
+		case "/apis":
+			list = &metav1.APIGroupList{
+				Groups: []metav1.APIGroup{
+					{
+						Name: "networking.istio.io",
+						Versions: []metav1.GroupVersionForDiscovery{
+							{GroupVersion: "v1alpha3", Version: "v1alpha3"},
+							{GroupVersion: "v1alpha3", Version: "v1alpha3"},
+						},
+					},
+				},
+			}
+		default:
+			t.Logf("unexpected request: %s", req.URL.Path)
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+		output, err := json.Marshal(list)
+		if err != nil {
+			t.Errorf("unexpected encoding error: %v", err)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(output)
+	}))
+
+	return server
+}
+
+func TestIstioEnabled(t *testing.T) {
+
+	server := initMockServer(t)
+	defer server.Close()
+
+	cfg := &restclient.Config{Host: server.URL}
+	r, e := istio.CheckIstioEnabled(cfg)
+	if r != true {
+		t.Fail()
+		t.Error(e)
+	}
+}
