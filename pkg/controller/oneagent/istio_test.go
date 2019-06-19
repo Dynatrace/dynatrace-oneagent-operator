756fd7d106a41db70121e9c687fbbabf1f7ab35a
namratachaudhary <chdhry.namrata@gmail.com>
2019-06-19 11:21:13 +0000

    add unit test

--- pkg/controller/oneagent/istio_test.go
+++ pkg/controller/oneagent/istio_test.go
@@ -6,11 +6,13 @@ import (
 
 	dynatracev1alpha1 "github.com/Dynatrace/dynatrace-oneagent-operator/pkg/apis/dynatrace/v1alpha1"
 	"github.com/Dynatrace/dynatrace-oneagent-operator/pkg/controller/istio"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	logf "sigs.k8s.io/controller-runtime/pkg/runtime/log"
 )
 
 var (
@@ -67,6 +69,23 @@ func TestReconcileOneAgent_ReconcileIstioViaDynatraceClient(t *testing.T) {
 	_, err := reconcileOA.Reconcile(req)
 	if err != nil {
 		t.Fatalf("error reconciling: %v", err)
+	}
+
+	// rerun reconcile instio configuration update
+	instance := &dynatracev1alpha1.OneAgent{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace,
+		},
+		Spec: *oa,
+	}
+	dtc, _ := mockBuildDynatraceClient(instance)
+	commHosts, _ := dtc.GetCommunicationHosts()
+	var log = logf.Log.WithName("oneagent.controller.test")
+
+	upd, err := reconcileOA.reconcileIstioConfigurations(log, instance, commHosts, "communication-endpoint")
+	if upd != true {
+		t.Error("expected true got false, communication endpoints needed to be updated")
 	}
 }
 
