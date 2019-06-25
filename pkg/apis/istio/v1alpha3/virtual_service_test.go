c496820cd28b9d1ff73bdab5f01686d78f5589e4
namratachaudhary <chdhry.namrata@gmail.com>
2019-06-25 13:05:43 +0200

    fix test name

--- pkg/apis/istio/v1alpha3/virtual_service_test.go
+++ pkg/apis/istio/v1alpha3/virtual_service_test.go
@@ -10,7 +10,7 @@ import (
 	istiov1alpha3 "istio.io/api/networking/v1alpha3"
 )
 
-func Test_Example(t *testing.T) {
+func Test_VirtualService(t *testing.T) {
 	buffer := bytes.NewBufferString("{\"apiVersion\":\"networking.istio.io/v1alpha3\",\"kind\":\"VirtualService\",\"metadata\":{\"clusterName\":\"\",\"creationTimestamp\":\"2018-11-26T03:19:57Z\",\"generation\":1,\"name\":\"test-virtual-service\",\"namespace\":\"istio-system\",\"resourceVersion\":\"1297970\",\"selfLink\":\"/apis/networking.istio.io/v1alpha3/namespaces/istio-system/virtualservices/test-virtual-service\",\"uid\":\"266fdacc-f12a-11e8-9e1d-42010a8000ff\"},\"spec\":{\"gateways\":[\"test-gateway\"],\"hosts\":[\"*\"],\"http\":[{\"match\":[{\"uri\":{\"prefix\":\"/\"}}],\"route\":[{\"destination\":{\"host\":\"test-service\",\"port\":{\"number\":8080}}}],\"timeout\":\"10s\"}]}}\n")
 
 	vs := VirtualService{}
