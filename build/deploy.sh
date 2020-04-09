1151f1474450870921322586628b996698b6b66a
Marco Mader <marco.mader@dynatrace.com>
2020-04-09 08:09:08 +0200

    Adapt if condition

--- build/deploy.sh
+++ build/deploy.sh
@@ -3,7 +3,7 @@
 if [[ "$GCR" == "true" ]]; then
     echo "$GCLOUD_SERVICE_KEY" | base64 -d | docker login -u _json_key --password-stdin https://gcr.io
     gcloud --quiet config set project "$GCP_PROJECT"
-else if [[ "$IMAGE" != "$OAO_IMAGE_RHCC_SCAN" ]]; then
+elif [[ "$IMAGE" != "$OAO_IMAGE_RHCC_SCAN" ]]; then
     TAG=$TAG-$TRAVIS_CPU_ARCH
 fi
 
