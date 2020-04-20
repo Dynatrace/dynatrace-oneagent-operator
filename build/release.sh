8bccefe773748f9f7bae43496f2753ab13e4099c
Marco Mader <marco.mader@dynatrace.com>
2020-04-20 11:33:31 +0200

    Prepare release 0.7.1

--- build/release.sh
+++ build/release.sh
@@ -1,5 +1,7 @@
 template_image="dynatrace-oneagent-operator:snapshot"
 current_image="dynatrace-agent-operator:${TRAVIS_TAG}"
 mkdir artefacts
-sed "s/${template_image}/${current_image}/g" deploy/kubernetes.yaml > artefacts/kubernetes.yaml
-sed "s/docker.io\/dynatrace\/${template_image}/registry.connect.redhat.com\/dynatrace\/${current_image}/g" deploy/openshift.yaml > artefacts/openshift.yaml
+# sed "s/${template_image}/${current_image}/g" deploy/kubernetes.yaml > artefacts/kubernetes.yaml
+# sed "s/docker.io\/dynatrace\/${template_image}/registry.connect.redhat.com\/dynatrace\/${current_image}/g" deploy/openshift.yaml > artefacts/openshift.yaml
+cp deploy/kubernetes.yaml artefacts/kubernetes.yaml
+cp deploy/openshift.yaml artefacts/openshift.yaml
