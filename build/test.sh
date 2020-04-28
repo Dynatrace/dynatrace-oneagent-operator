db1b7998e6a65e48a663ea20a41d76e629e3530e
Marco Mader <marco.mader@dynatrace.com>
2020-04-28 15:50:35 +0200

    Update kubebuilder version for integration tests

--- build/test.sh
+++ build/test.sh
@@ -6,7 +6,7 @@ sudo chmod 777 /usr/local/kubebuilder
 
 ########## Fetch Kubebuilder ##########
 if [ ! -d "/usr/local/kubebuilder/bin" ]; then
-    curl -L https://github.com/kubernetes-sigs/kubebuilder/releases/download/v1.0.8/kubebuilder_1.0.8_linux_amd64.tar.gz -o kubebuilder.tar.gz
+    curl -L https://github.com/kubernetes-sigs/kubebuilder/releases/download/v2.3.1/kubebuilder_2.3.1_linux_amd64.tar.gz -o kubebuilder.tar.gz
     tar -zxvf kubebuilder.tar.gz --strip-components=1 -C /usr/local/kubebuilder
 fi
 
