b2fdbb85422c554cc77b49ce9781945dd51e1bf1
Luis Garcia <luis.garcia@dynatrace.com>
2020-08-05 12:08:03 +0200

    Copy OneAgent package before container stops

--- pkg/controller/namespace/namespace_controller_test.go
+++ pkg/controller/namespace/namespace_controller_test.go
@@ -113,9 +113,10 @@ fi
 echo "Unpacking OneAgent package..."
 if ! unzip -o -d "${target_dir}" "${archive}"; then
 	echo "Failed to unpack the OneAgent package."
+	mv "${archive}" "${target_dir}/package.zip"
 	exit 0
 fi
-mv "${archive}" "${target_dir}/package.zip"
+rm -f "${archive}"
 
 echo "Configuring OneAgent..."
 echo -n "${INSTALLPATH}/agent/lib64/liboneagentproc.so" >> "${target_dir}/ld.so.preload"
