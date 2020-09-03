e8f673db7f623fd64e35b99ede166456789ac5a8
Luis Garcia <luis.garcia@dynatrace.com>
2020-09-03 07:30:46 +0200

    Sped up TravisCI pipeline (#310)

--- build/release.sh
+++ build/release.sh
@@ -1,12 +1,11 @@
-curl -s "https://raw.githubusercontent.com/\
-kubernetes-sigs/kustomize/master/hack/install_kustomize.sh"  | bash
+curl -s "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh" | bash
 
 template_image="dynatrace-oneagent-operator:snapshot"
 current_image="dynatrace-agent-operator:${TRAVIS_TAG}"
 mkdir artefacts
 
-kustomize build github.com/Dynatrace/dynatrace-oneagent-operator/deploy/kubernetes -o kubernetes.yaml
-kustomize build github.com/Dynatrace/dynatrace-oneagent-operator/deploy/openshift -o openshift.yaml
+./kustomize build github.com/Dynatrace/dynatrace-oneagent-operator/deploy/kubernetes -o kubernetes.yaml
+./kustomize build github.com/Dynatrace/dynatrace-oneagent-operator/deploy/openshift -o openshift.yaml
 
 sed "s/${template_image}/${current_image}/g" kubernetes.yaml > artefacts/kubernetes.yaml
 sed "s/docker.io\/dynatrace\/${template_image}/registry.connect.redhat.com\/dynatrace\/${current_image}/g" openshift.yaml > artefacts/openshift.yaml
