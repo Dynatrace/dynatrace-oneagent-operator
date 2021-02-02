3a934af330589bc426e7765ced678e82a3d8b314
Michael Rynkiewicz <michael.rynkiewicz@dynatrace.com>
2021-02-02 08:28:38 +0100

    testing

--- build/test.sh
+++ build/test.sh
@@ -15,9 +15,13 @@ fi
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
 echo "$(<kubectl.sha256) kubectl" | sha256sum --check
+chmod +x kubectl
 mkdir -p ~/.local/bin/kubectl
 mv ./kubectl "${HOME}/.local/bin/kubectl"
 export PATH="${HOME}/.local/bin/kubectl:$PATH"
+
+echo "$PATH"
+whereis kubectl
 
 ########## Get kube-config and install kubectl ##########
 
