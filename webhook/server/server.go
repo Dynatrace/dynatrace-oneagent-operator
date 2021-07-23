60c2bbaeb05406bb96fd44d9435a58971a847243
rspierz <rspierz@gmail.com>
2021-07-23 13:17:23 -0400

    Change pull policy to ifnotpresent
    Allow image to be pulled directly from node instead of pulled from registry each time. Save on pull time and also provides a level of workflow if registry is unavailable.

--- webhook/server/server.go
+++ webhook/server/server.go
@@ -177,7 +177,7 @@ func (m *podInjector) Handle(ctx context.Context, req admission.Request) admissi
 	ic := corev1.Container{
 		Name:            "install-oneagent",
 		Image:           image,
-		ImagePullPolicy: corev1.PullAlways,
+		ImagePullPolicy: corev1.PullIfNotPresent,
 		Command:         []string{"/usr/bin/env"},
 		Args:            []string{"bash", "/mnt/config/init.sh"},
 		Env: []corev1.EnvVar{
